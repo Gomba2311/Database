3bd8d745b3839d47a182ecf2af999942ee39fc0a
Norbi PC <norbert.paucsik2010@gmai.com>
2024-03-30 08:37:01 +0200

    második már majdnem kész a save és a properties print nincs még kész

--- mainOOP.py
+++ mainOOP.py
@@ -14,7 +14,7 @@ class MainWindow:
         # --|--main area--|--
         main_frame = tk.Frame(master, bg="yellow")
         # @@
-        main_label = tk.Label(main_frame, text="Adatbázis", font=("Comic Sans MS", 20), bg="yellow", fg="red")
+        main_label = tk.Label(main_frame, text="  Adatbázis  ", font=("Comic Sans MS", 20), bg="yellow", fg="red")
         main_label.pack()
         # @@
         main_frame.grid(row=1, column=2)
@@ -37,6 +37,16 @@ class MainWindow:
         # @@
         create_label = tk.Label(create_frame, text="Create", font=("Comic Sans MS", 18), bg="yellow", fg="red")
         create_label.pack()
+        self.create_hiv_entry = tk.Entry(create_frame, width="30", font=("Comic Sans MS", 12))
+        self.create_name_entry = tk.Entry(create_frame, width="30", font=("Comic Sans MS", 12))
+        self.create_age_entry = tk.Entry(create_frame, width="30", font=("Comic Sans MS", 12))
+        self.create_gender_entry = tk.Entry(create_frame, width="30", font=("Comic Sans MS", 12))
+        # -- packing --
+        self.create_hiv_entry.pack()
+        self.create_name_entry.pack()
+        self.create_age_entry.pack()
+        self.create_gender_entry.pack()
+        # -------------
         create_button = tk.Button(create_frame, text="Create", command=self.create, font=('Arial', 15), bg="lightgreen")
         create_button.pack()
         # @@
@@ -44,36 +54,47 @@ class MainWindow:
 
         # --|--load trying--|--
         try:
-            with open("Assets/persons.txt") as pl:
-                self.személyek = js.load(pl)
+            with open("Assets/hivatkozasok.txt") as pl:
+                self.hiv = js.load(pl)
         except:
-            pass
+            self.hiv = []
         self.peron = None
+        try:
+            with open("Assets/szemelyek.txt") as nl:
+                self.nevek = js.load(pl)
+        except:
+            self.nevek = []
 
     # --|-- search definiton--|--
     def search(self):
         nothuman = True
         human = False
         try:
-            for i in range(len(self.személyek)):
-                if self.személyek[i] == self.search_entry.get():
+            for i in range(len(self.hiv)):
+                if self.hiv[i] == self.search_entry.get():
                     self.peron = i
-                    FailedLabel = tk.Label(self.mast, text=self.személyek[i], font=("Georga", 20), bg="yellow", fg="green")
-                    FailedLabel.grid(row=5, padx=350, column=1)
+                    FailedLabel = tk.Label(self.mast, text=self.nevek[i], font=("Georga", 20), bg="yellow", fg="green")
+                    FailedLabel.grid(row=5, column=1)
                     nothuman = False
                     human = True
-                elif self.személyek[i] != self.search_entry.get():
+                elif self.hiv[i] != self.search_entry.get():
                     nothuman = True
-                if i == len(self.személyek) - 1 and nothuman and not human:
+                if i == len(self.hiv) - 1 and nothuman and not human:
                     FailedLabel = tk.Label(self.mast, text="Nem létező személy!", font=("Georga", 20), bg="yellow", fg="red")
                     FailedLabel.grid(row=5, padx=350, column=1)
         except:
             FailedLabel = tk.Label(self.mast, text="Hiányzó Adat!", font=("Georga", 18), bg="yellow", fg="red")
             FailedLabel.grid(row=3, column=2)
+    def new_member(self, nev, kor, gend):
+        nmn = nev
+        nma = kor
+        nmg = gend
+        nmp = {nmn, nma, nmg}
+        self.nevek.append(nmp)
 
     def create(self):
-        pass
-
+        self.hiv.append(self.create_hiv_entry.get())
+        self.new_member(self.create_name_entry.get, self.create_age_entry.get, self.create_gender_entry.get)
 
 
 
